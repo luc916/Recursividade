e826841b499b928b864681e1e1f91b1b98d37c7e
luc916 <lucaskenzoni@gmail.com>
2025-08-05 10:05:23 -0300

    Soma ajustado

--- soma.c
+++ soma.c
@@ -1,4 +1,14 @@
+#include <stdio.h>
 int soma(int n) {
     if (n == 0) return 0;
     return n + soma(n - 1);
+}
+
+int main () {
+    int num;
+    printf("Digite um número positivo: ");
+    scanf("%d", &num);
+    printf("Soma recursiva do número %d = %d", num, soma (num));
+    printf("\n");
+    return 0;
 }
