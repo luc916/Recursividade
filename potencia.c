64d3a1ce589b814b8b74e33336f2482866f0a5c3
luc916 <lucaskenzoni@gmail.com>
2025-08-05 10:15:26 -0300

    Potencia ajustado

--- potencia.c
+++ potencia.c
@@ -1,4 +1,16 @@
+#include <stdio.h>
 int potencia(int base, int expoente) {
     if (expoente == 0) return 1;
     return base * potencia(base, expoente - 1);
+}
+
+int main () {
+    int num1, num2;
+    printf("Digite um número como base: ");
+    scanf("%d", &num1);
+    printf("Digite um número como expoente: ");
+    scanf("%d", &num2);
+    printf("A potência de %d elevado a %d é igual a %d", num1, num2, potencia(num1, num2));
+    printf("\n");
+    return 0;
 }
